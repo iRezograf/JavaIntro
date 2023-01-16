b6ff3de6d820b1fad3b5a9413aed1cc2495bbe49
Rafael Rezyapov <Rezograf@gmail.com>
2023-01-16 21:11:18 +0300

    add Map<String, Long> wordCount

--- JavaIntro/src/test/java/rra/javaintro/Streams.java
+++ JavaIntro/src/test/java/rra/javaintro/Streams.java
@@ -8,30 +8,44 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.UUID;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 public class Streams {
     @Test
     public void creation() throws IOException {
+        String f = "/Users/rafaelrezyapov/IdeaProjects/JavaIntro/src/test/java/rra/javaintro/Streams.java";
         ArrayList<String> aa = new ArrayList<>();
-        Stream<String> lines = Files.lines(Paths.get("/Users/rafaelrezyapov/IdeaProjects/JavaIntro/src/test/java/rra/javaintro/Streams.java"));
+        ArrayList<String> bb = new ArrayList<>();
+        Stream<String> lines = Files.lines(Paths.get(f));
         lines.forEach(aa::add);
+
         for (String item: aa) {
-            System.out.println(item);
+            String[] w = item.split(" ",0);
+            for (String a : w)
+                bb.add(a);
         }
+        //bb.forEach(n-> System.out.println(n));
+        Map<String, Long> wordCount = bb.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder())
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (e1, e2) -> e2, LinkedHashMap::new));
+        System.out.println(wordCount);
 
 
         Stream<Path> list = Files.list(Paths.get("./"));
-        list.forEach(s-> System.out.println(s));
-        System.out.println();
+        //list.forEach(s-> System.out.println(s));
+        //System.out.println();
 
         Stream<Path> walk = Files.walk(Paths.get("./"), 3);
-        walk.forEach(s-> System.out.println(s));
-        System.out.println();
+        //walk.forEach(s-> System.out.println(s));
+        //System.out.println();
 
         IntStream intStream = IntStream.of(1, 2, 3, 4);
         DoubleStream doubleStream = DoubleStream.of(1.2, 3.4);
