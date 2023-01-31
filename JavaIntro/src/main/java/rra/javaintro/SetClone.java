05f945775435835cb1b2001004b5e9c0ed291534
Rafael Rezyapov <Rezograf@gmail.com>
2023-01-31 22:28:44 +0300

    add SetClone.java

---
+++ JavaIntro/src/main/java/rra/javaintro/SetClone.java
@@ -0,0 +1,52 @@
+package rra.javaintro;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class SetClone {
+    private static Object OBJECT;
+    private Map<Integer, Object> setClone;
+
+    public SetClone() {
+        this.setClone = new HashMap<>();
+    }
+
+    public static void main(String[] args) {
+//  Разработать программу, имитирующую поведение коллекции HashSet.
+//  В программе содать метод add добавляющий элемент,
+//  и метод позволяющий выводить эллементы коллекции в консоль.
+//  Формат данных Integer.
+//    Set<Integer> set = new HashSet<>();
+//    set.add(22);
+//    set.add(82);
+//    set.add(224);
+//    set.add(224);
+//    set.add(212);
+//    set.forEach(integer -> System.out.println(integer));
+
+        SetClone pseudoSet = new SetClone();
+        pseudoSet.add(22);
+        pseudoSet.add(82);
+        pseudoSet.add(224);
+        pseudoSet.add(224);
+        pseudoSet.add(212);
+
+        System.out.println(pseudoSet);
+    }
+
+    public void add(Integer val) {
+        this.setClone.put(val, OBJECT);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        this.setClone.forEach((key, value) -> sb.append(key).append(" "));
+        return sb.toString();
+    }
+
+}
+
